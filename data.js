75af408771a95d8a8b5434c0781f03ba820e1054
snu404 <53073565+snu404@users.noreply.github.com>
2026-03-02 22:11:01 +0900

    Update data.js

--- data.js
+++ data.js
@@ -52,9 +52,12 @@ const APP_DATA = {
         {
           time: "09:00~10:20",
           sessions: [
-            { name: "튜토리얼 I (금속-유기 구조체: 나노에서 마이크로까지)", room: "203", note: "고려대학교 김현욱 교수", talks: [] },
-            { name: "튜토리얼 II (유체소자 개발을 위한 전통적/비전통적 마이크로/나노구조 제작)", room: "201A", note: "전남대학교 배주열 교수", talks: [] },
-            { name: "튜토리얼 III (저차원 나노미터 공간에서의 물질 전달 현상)", room: "201B", note: "포항공과대학교 박형규 교수", talks: [] }
+            { name: "튜토리얼 I", room: "203", note: "", talks: [{kind: "", title: "금속-유기 구조체: 나노에서 마이크로까지",
+      speaker_affil: "고려대학교",speaker_name: "김현욱 교수", noPdf: true}] },
+            { name: "튜토리얼 II", room: "201A", note: "", talks: [{kind: "", title: "유체소자 개발을 위한 전통적/비전통적 마이크로/나노구조 제작",
+      speaker_affil: "전남대학교",speaker_name: "배주열 교수", noPdf: true}] },
+            { name: "튜토리얼 III", room: "201B", note: "", talks: [{kind: "", title: "저차원 나노미터 공간에서의 물질 전달 현상",
+      speaker_affil: "포항공과대학교",speaker_name: "박형규 교수", noPdf: true}] }
           ]
         },
         {
